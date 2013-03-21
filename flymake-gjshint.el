ee5d69c98b16d4d97db9a2fcdf4129c0d6f46b88
Yasuyuki Oka <yasuyk@gmail.com>
2013-03-21 14:51:56 +0900

    Bump version to 0.0.2

--- flymake-gjshint.el
+++ flymake-gjshint.el
@@ -4,7 +4,7 @@
 
 ;; Author: Yasuyuki Oka <yasuyk@gmail.com>
 ;; Keywords: flymake, javascript, jshint, gjslint
-;; Version: 0.0.1
+;; Version: 0.0.2
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
